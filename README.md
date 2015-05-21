71c086f1f62471b383219515000fb4edcb2555e1
Kukoda János <kukodajanos@icloud.com>
2015-05-21 14:41:14 +0200

    REST OpenShift

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-The OpenShift `nodejs` cartridge documentation can be found at:
+# REST-Node.js-MongoDB-OpenShift
 
-http://openshift.github.io/documentation/oo_cartridge_guide.html#nodejs
+REST-Node.js-MongoDB-OpenShift is a super easy Node.js script that offer a RESTful storage in a MongoDB database. Script is 'optimized' to run on RedHad OpenShift cloud platform. I didn't wanted to maintain any private server or a separate database - webserver combo, but a solution that is free for prototypes and all the required backend components are covered. The future plan is to implement the following features. Feel free to help me, by forking the project and adding you own code. My endgoal to have a multiplatform CloudKit like solution, developed by Apple.
+
+  - CASCADE DELETE based on some kind of schema
+  - fetch record changes by record type
+  - assets
--- server.js
+++ server.js
@@ -1,159 +1,189 @@
-#!/bin/env node
-//  OpenShift sample Node application
-var express = require('express');
-var fs      = require('fs');
+  var http = require('http');
+  var express = require('express');
+  var app = express();
+  var mongodb = require("mongodb");
+  var connection = require('./connection');
+  var bson = require("bson");
+  var BSON = bson.BSONPure; //
 
+  app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 8080);
+  app.set('ip', process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1');
+  app.use(express.bodyParser());
 
-/**
- *  Define the sample application.
- */
-var SampleApp = function() {
+  var connection_string = 'mongodb://127.0.0.1:27017/nodejs';
+  // if OPENSHIFT env variables are present, use the available connection info:
+  if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
+    connection_string = 'mongodb://' + process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+    process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+    process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+    process.env.OPENSHIFT_MONGODB_DB_PORT;
+  }
 
-    //  Scope.
-    var self = this;
+  http.createServer(app).listen(app.get('port'), app.get('ip'), function(){
+    console.log('Express server listening on port ' + app.get('port'));
+  });
 
+  app.get('/:db/:collection/:id?', function (req, res) {
 
-    /*  ================================================================  */
-    /*  Helper functions.                                                 */
-    /*  ================================================================  */
+    var query = req.query.query ? JSON.parse(req.query.query) : {};
 
-    /**
-     *  Set up server IP address and port # using env variables/defaults.
-     */
-    self.setupVariables = function() {
-        //  Set the environment variables we need.
-        self.ipaddress = process.env.OPENSHIFT_NODEJS_IP;
-        self.port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+    if (req.params.id) {
+      query = {'_id': new BSON.ObjectID(req.params.id)};
+    }
+    var options = req.params.options || {};
 
-        if (typeof self.ipaddress === "undefined") {
-            //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
-            //  allows us to run/test the app locally.
-            console.warn('No OPENSHIFT_NODEJS_IP var, using 127.0.0.1');
-            self.ipaddress = "127.0.0.1";
-        };
-    };
+    var test = ['limit','sort','fields','skip','hint','explain','snapshot','timeout'];
 
+    for( o in req.query ) {
+      if( test.indexOf(o) >= 0 ) {
+        options[o] = req.query[o];
+      } 
+    }
 
-    /**
-     *  Populate the cache.
-     */
-    self.populateCache = function() {
-        if (typeof self.zcache === "undefined") {
-            self.zcache = { 'index.html': '' };
+    connection.connect(connection_string + '/' + req.params.db + '?authSource=admin', function (err, db) {
+
+      if (err) {
+
+        console.log('Db open error: ' + err.message);
+        res.status(500).json({ message: 'Server error' });
+        return;
+      }
+
+      db.collection(req.params.collection, function(err, collection) {
+        if (err) {
+          //logger.error('Error getting collection ' + collection + ': ' + err.message);
+          res.status(500).json({ message: 'Server error' });
+          return;
         }
 
-        //  Local cache for static content.
-        self.zcache['index.html'] = fs.readFileSync('./index.html');
-    };
+        collection.find(query, options, function(err, cursor) {
+          if (err) {
+            //logger.error('Error finding document(s): ' + err.message);
+            res.status(500).json({ message: 'Server error' });
+            return;
+          }
 
+          cursor.toArray(function(err, docs){
+            if (err) {
+              //logger.error('Error getting database cursor as array: ' + err.message);
+              res.status(500).json({ message: 'Server error' });
+              return;
+            } 
 
-    /**
-     *  Retrieve entry (content) from cache.
-     *  @param {string} key  Key identifying content to retrieve from cache.
-     */
-    self.cache_get = function(key) { return self.zcache[key]; };
-
-
-    /**
-     *  terminator === the termination handler
-     *  Terminate server on receipt of the specified signal.
-     *  @param {string} sig  Signal to terminate on.
-     */
-    self.terminator = function(sig){
-        if (typeof sig === "string") {
-           console.log('%s: Received %s - terminating sample app ...',
-                       Date(Date.now()), sig);
-           process.exit(1);
-        }
-        console.log('%s: Node server stopped.', Date(Date.now()) );
-    };
-
-
-    /**
-     *  Setup termination handlers (for exit and a list of signals).
-     */
-    self.setupTerminationHandlers = function(){
-        //  Process on exit and signals.
-        process.on('exit', function() { self.terminator(); });
-
-        // Removed 'SIGPIPE' from the list - bugz 852598.
-        ['SIGHUP', 'SIGINT', 'SIGQUIT', 'SIGILL', 'SIGTRAP', 'SIGABRT',
-         'SIGBUS', 'SIGFPE', 'SIGUSR1', 'SIGSEGV', 'SIGUSR2', 'SIGTERM'
-        ].forEach(function(element, index, array) {
-            process.on(element, function() { self.terminator(element); });
+            var result = [];          
+            if(req.params.id) {
+              if(docs.length > 0) {
+                result = docs[0];
+                res.json(result);
+              } else {
+                res.status(404).json({ ok: 0 });
+              }
+            } else {
+              docs.forEach(function(doc) {
+                result.push(doc);
+              });
+              res["json"](result);
+            }
+          });
         });
-    };
+      });
+    });
+  });
 
+  app.put('/:db/:collection/:id', function(req, res) {
+    var spec = {'_id': new BSON.ObjectID(req.params.id)};
 
-    /*  ================================================================  */
-    /*  App server functions (main app logic here).                       */
-    /*  ================================================================  */
+        connection.connect(connection_string + '/' + req.params.db + '?authSource=admin', function (err, db) {
 
-    /**
-     *  Create the routing table entries + handlers for the application.
-     */
-    self.createRoutes = function() {
-        self.routes = { };
+      if (err) {
+        //logger.error('Db open error: ' + err.message);
+        res.status(500).json({ message: 'Server error' });
+        return;
+      }
 
-        self.routes['/asciimo'] = function(req, res) {
-            var link = "http://i.imgur.com/kmbjB.png";
-            res.send("<html><body><img src='" + link + "'></body></html>");
-        };
-
-        self.routes['/'] = function(req, res) {
-            res.setHeader('Content-Type', 'text/html');
-            res.send(self.cache_get('index.html') );
-        };
-    };
-
-
-    /**
-     *  Initialize the server (express) and create the routes and register
-     *  the handlers.
-     */
-    self.initializeServer = function() {
-        self.createRoutes();
-        self.app = express.createServer();
-
-        //  Add handlers for the app (from the routes).
-        for (var r in self.routes) {
-            self.app.get(r, self.routes[r]);
+      db.collection(req.params.collection, function(err, collection) {
+        if (err) {
+          //logger.error('Error getting collection ' + collection + ': ' + err.message);
+          res.status(500).json({ message: 'Server error' });
+          return;
         }
-    };
 
-
-    /**
-     *  Initializes the sample application.
-     */
-    self.initialize = function() {
-        self.setupVariables();
-        self.populateCache();
-        self.setupTerminationHandlers();
-
-        // Create the express server and routes.
-        self.initializeServer();
-    };
-
-
-    /**
-     *  Start the server (starts up the sample application).
-     */
-    self.start = function() {
-        //  Start the app on the specific interface (and port).
-        self.app.listen(self.port, self.ipaddress, function() {
-            console.log('%s: Node server started on %s:%d ...',
-                        Date(Date.now() ), self.ipaddress, self.port);
+        collection.update(spec, req.body, true, function(err, docs) {
+          res.json({ ok: 1 });
         });
-    };
+      });
+    });
+  });
 
-};   /*  Sample Application.  */
+  app.post('/:db/:collection', function(req, res) {
 
+   if(req.body) {
 
+     console.log(connection_string + '/' + req.params.db + '?authSource=admin');
 
-/**
- *  main():  Main code.
- */
-var zapp = new SampleApp();
-zapp.initialize();
-zapp.start();
+     connection.connect(connection_string + '/' + req.params.db + '?authSource=admin', function (err, db) {
 
+      if (err) {
+
+        console.log('Db open error: ' + err.message);
+        res.status(500).json({ message: 'Server error' });
+        return;
+      }
+
+      db.collection(req.params.collection, function(err, collection) {
+        if (err) {
+          console.log('Error getting collection ' + collection + ': ' + err.message);
+          res.status(500).json({ message: 'Server error' });
+          return;
+        }
+
+        collection.insert(
+          Array.isArray(req.body) ? req.body[0] : req.body,
+          function(err, docs) {
+            if (err) {
+
+              console.log('Error inserting into collection ' + collection + ': ' + err.message);
+              res.status(500).json({ message: 'Server error' });
+              return;
+            }
+
+            res.header('Content-Type', 'application/json');
+            res.status(201).json({ message: 'ok' });
+          }
+          );
+      });
+    });
+   } else {
+     res.json({ ok: 1 });
+   }
+ });
+
+  app.delete('/:db/:collection/:id', function(req, res) {
+    var spec = {'_id': new BSON.ObjectID(req.params.id)};
+
+    connection.connect(connection_string + '/' + req.params.db + '?authSource=admin', function (err, db) {
+
+      if (err) {
+        //logger.error('Db open error: ' + err.message);
+        res.status(500).json({ message: 'Server error' });
+        return;
+      }
+
+      db.collection(req.params.collection, function(err, collection) {
+        if (err) {
+          //logger.error('Error getting collection ' + collection + ': ' + err.message);
+          res.status(500).json({ message: 'Server error' });
+          return;
+        }
+
+        collection.remove(spec, function(err, docs) {
+          if (err) {
+            //logger.error('Error removing from ' + collection + ': ' + err.message);
+            res.status(500).json({ message: 'Server error' });
+            return;
+          }
+          res.json({ ok: 1 });
+        });
+      });
+    });
+  });
